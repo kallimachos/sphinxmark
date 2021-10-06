a0c5753b8fae2e6d534f0e6079ec7e77855d974d
Brian Moss <kallimachos@gmail.com>
2021-10-06 14:18:56 +1000

    Move to 1.0.0 stable release (#39)
    - Replace Travis-CI with GitHub action.

--- setup.py
+++ setup.py
@@ -11,40 +11,42 @@ from setuptools import setup
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='sphinxmark',
-    version='0.2.1',
-    description='A Sphinx extension that enables watermarks for HTML output.',
+    name="sphinxmark",
+    version="0.2.1",
+    description="A Sphinx extension that enables watermarks for HTML output.",
     long_description=long_description,
-    url='https://github.com/kallimachos/sphinxmark',
-    author='Brian Moss',
-    author_email='kallimachos@gmail.com',
-    license='Apache 2.0',
+    url="https://github.com/kallimachos/sphinxmark",
+    author="Brian Moss",
+    author_email="kallimachos@gmail.com",
+    license="Apache 2.0",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Documentation :: Sphinx',
-        'Framework :: Sphinx :: Extension',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Documentation :: Sphinx",
+        "Framework :: Sphinx :: Extension",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-
-    keywords='sphinx documentation watermark',
-
-    packages=['sphinxmark'],
-
+    keywords="sphinx documentation watermark",
+    packages=["sphinxmark"],
     package_data={
-        'sphinxmark': ['watermark-draft.png', 'border.tpl',
-                       'watermark.tpl', 'arial.ttf'],
+        "sphinxmark": [
+            "watermark-draft.png",
+            "border.tpl",
+            "watermark.tpl",
+            "arial.ttf",
+        ],
     },
-
     install_requires=[
-        'bottle',
-        'Pillow',
-        'sphinx',
+        "bottle",
+        "Pillow",
+        "sphinx",
     ],
 )
